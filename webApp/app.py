36e2ecec9573baf5b3831a003c67d48492b3f9de
Yumaev <art.yumaev@gmail.com>
2020-02-29 23:19:54 +0300

    upd

--- webApp/app.py
+++ webApp/app.py
@@ -7,32 +7,32 @@ app = Flask(__name__)
 # Home
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Home')
 
 # Collection
 @app.route('/collection/')
 def collection():
-    return render_template('collection.html')
+    return render_template('collection.html', title='Collection')
 
 # About us
 @app.route('/about_us/')
 def about_us():
-    return render_template('about_us.html')
+    return render_template('about_us.html', title='About us')
 
 # Cart
 @app.route('/cart/')
 def cart():
-    return render_template('cart.html')
+    return render_template('cart.html', title='Cart')
 
 # Login
 @app.route('/login/')
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Log in')
 
 # Item
 @app.route('/item/<vendor>')
 def item(vendor):
-    return escape(vendor)
+    return render_template('item.html', vendor=vendor)
 
 
 if __name__ == '__main__':
