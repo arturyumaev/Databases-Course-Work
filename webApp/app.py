ba4add9e17492967ce8598f4e7c206baf9420f60
Yumaev <art.yumaev@gmail.com>
2020-02-29 14:58:36 +0300

    добавил страницы коллекции, логина и корзины. изменил шапку

--- webApp/app.py
+++ webApp/app.py
@@ -8,24 +8,26 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+# Collection
+@app.route('/collection/')
+def collection():
+    return render_template('collection.html')
+
 # About us
 @app.route('/about_us/')
 def about_us():
     return render_template('about_us.html')
 
-# Male
-@app.route('/male/')
-@app.route('/male/<category>/<vendor>')
-def male(category=None, vendor=None):
-    if category != None and vendor != None:
-        return category + '/' + vendor
+# Cart
+@app.route('/cart/')
+def cart():
+    return render_template('cart.html')
 
-    return 'Male'
+# Login
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
-# Female
-@app.route('/female/')
-def female():
-    return 'Female'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
