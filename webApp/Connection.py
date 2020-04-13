0d6e1cf707636df8ea1fb6e03cabdf40cbc27193
yumaevartur <art.yumaev@gmail.com>
2020-04-13 17:19:58 +0300

    добавил обработку ошибок соединения к БД

--- webApp/Connection.py
+++ webApp/Connection.py
@@ -4,9 +4,31 @@ from .ConfigManager import ConfigManager
 class Connection():
     def __init__(self):
         self.getConfig()
+        self.instance = None
 
     def getConfig(self):
         self.api, self.path = ConfigManager().readConfig()
 
     def getInstance(self):
-        return self.api.connect(self.path)
+        try:
+            print("Trying to connect to database.")
+            self.instance = self.api.connect(self.path)
+        except:
+            print("An error occured while connecting to database")
+            print("Trying to reconnect...")
+            for i in range(1, 4):
+                print("Attempt to connect...", i)
+                try:
+                    self.instance = self.api.connect(self.path)
+                except TimeoutError:
+                    print("Can't connect.")
+        finally:
+            if self.instance == None:
+                print("Couldn't connect to database")
+                raise RuntimeError
+            else:
+                print("A connection to the database has been established")
+                
+                return self.instance
+
+
