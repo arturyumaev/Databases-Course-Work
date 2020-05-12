980f98ffc48ad88664a474373a8974acd5294672
Artur Yumaev <art.yumaev@gmail.com>
2020-05-12 23:51:49 +0300

    update cart

--- webApp/BusinessRules/Cart.py
+++ webApp/BusinessRules/Cart.py
@@ -18,36 +18,41 @@ class Cart:
         self.totalOrderPrice = 0
 
     def addItem(self, vendor, size, price):
-        if vendor in self.items:
-            if size in self.items[vendor]["sizes"]:
-                self.items[vendor]["sizes"][size] += 1
+        try:
+            if vendor in self.items:
+                if size in self.items[vendor]["sizes"]:
+                    self.items[vendor]["sizes"][size] += 1
+                else:
+                    self.items[vendor]["sizes"][size] = 1
             else:
-                self.items[vendor]["sizes"][size] = 1
-        else:
-            self.items[vendor] = {
-                "price": price,
-                "sizes" : {
-                    size: 1
+                self.items[vendor] = {
+                    "price": price,
+                    "sizes" : {
+                        size: 1
+                    }
                 }
-            }
-        self.totalOrderPrice += price
-        self.itemsQuantity += 1
-        self.status = self.statusCodes[1]
+            self.totalOrderPrice += price
+            self.itemsQuantity += 1
+            self.status = self.statusCodes[1]
+        except IOError:
+            print("An error occured while creating new item in cart for user", self.userId)
 
     def removeItem(self, vendor, size, price):
-        self.itemsQuantity -= self.items[vendor]["sizes"][size]
-        self.totalOrderPrice -= self.items[vendor]["sizes"][size] * price
-        del self.items[vendor]["sizes"][size]
+        try:
+            self.itemsQuantity -= self.items[vendor]["sizes"][size]
+            self.totalOrderPrice -= self.items[vendor]["sizes"][size] * price
+            del self.items[vendor]["sizes"][size]
+        except IOError:
+            print("An error occured while deleting item from cart for user", self.userId)
 
     def updateItemAmount(self, vendor, size, method):
-        if vendor in self.items and size in self.items[vendor]["sizes"]:
-            updateMode = 1 if method == "inc" else -1
-            self.items[vendor]["sizes"][size] += updateMode
-            self.itemsQuantity += updateMode
-            self.totalOrderPrice += updateMode * self.items[vendor]["price"]
+        try:
+            if vendor in self.items and size in self.items[vendor]["sizes"]:
+                updateMode = 1 if method == "inc" else -1
+                self.items[vendor]["sizes"][size] += updateMode
+                self.itemsQuantity += updateMode
+                self.totalOrderPrice += updateMode * self.items[vendor]["price"]
 
-        return self.items[vendor]["sizes"][size], self.itemsQuantity, self.totalOrderPrice
-
-    def confirmOrderAndSendRequest(self):
-        # confirm ...
-        self.status = self.statusCodes[2]
+            return self.items[vendor]["sizes"][size], self.itemsQuantity, self.totalOrderPrice
+        except IOError:
+            print("An error occured while updating item quantity in cart for user", self.userId)
