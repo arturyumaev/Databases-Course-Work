8f703fa2ee10d65cfddfdce046e12acb0ad7a8e0
Artur Yumaev <art.yumaev@gmail.com>
2020-06-03 16:00:06 +0300

    добавил женские товары

---
+++ webApp/database/female.sql
@@ -0,0 +1,18 @@
+
+insert into goods (vendor, gender, photo, rating, price, discount, discount_bool, new, description, category, color)
+values
+    (0063, 'female', 'women/accessories/0063.jpg', 14, 2855 , 0, 0, 1, 'Bag', 'accessories', 'Blue'),
+    (0064, 'female', 'women/accessories/0064.jpg', 934, 4335, 0, 0, 1, 'Backpack', 'accessories', 'Blue'),
+    (0065, 'female', 'women/accessories/0065.jpg', 240, 2548, 0, 0, 0, 'Glasses', 'accessories', 'Red'),
+    (0066, 'female', 'women/outerwear/0066.jpg', 333, 35768 , 0, 0, 0, 'Jacket', 'outerwear', 'Blue'),
+    (0067, 'female', 'women/outerwear/0067.jpg', 259, 32266 , 0, 0, 0, 'Down Jacket', 'outerwear', 'Green'),
+    (0068, 'female', 'women/outerwear/0068.jpg', 435, 23524 , 0, 0, 1, 'Jacket', 'outerwear', 'Grey'),
+    (0069, 'female', 'women/pants/0069.jpg', 718, 475     , 0.1, 1, 0, 'Jeans', 'pants', 'Blue'),
+    (0070, 'female', 'women/pants/0070.jpg', 1300, 742    , 0.1, 1, 0, 'Jeans', 'pants', 'Blue'),
+    (0071, 'female', 'women/pants/0071.jpg', 942, 628       , 0, 0, 0, 'Jeans', 'pants', 'Black'),
+    (0072, 'female', 'women/shoes/0072.jpg', 251, 524     , 0.2, 1, 1, 'Shoes', 'shoes', 'Brown'),
+    (0073, 'female', 'women/shoes/0073.jpg', 460, 654       , 0, 0, 0, 'Sneakers', 'shoes', 'White'),
+    (0074, 'female', 'women/shoes/0074.jpg', 670, 554       , 0, 0, 1, 'Shoes', 'shoes', 'Black'),
+    (0075, 'female', 'women/tshirts/0075.jpg', 964, 275     , 0, 0, 0, 'T-Shirt', 'tshirts', 'White'),
+    (0076, 'female', 'women/tshirts/0076.jpg', 346, 207   , 0.3, 1, 1, 'T-Shirt', 'tshirts', 'Black'),
+    (0077, 'female', 'women/tshirts/0077.jpg', 53, 202      , 0, 0, 0, 'T-Shirt', 'tshirts', 'White');
--- webApp/database/generate_availability.py
+++ webApp/database/generate_availability.py
@@ -1,11 +1,11 @@
 import random
 
-out = open("./avail_male.sql", 'w')
+out = open("./avail_female.sql", 'w')
 
 random.seed(15)
 
 inputs = []
-for vendor in range(1, 63):
+for vendor in range(63, 78):
     for size in [44, 46, 48, 50, 52]:
         nsz = random.randint(3, 10)
         s = '    ({0}, {1}, {2})'.format(vendor, size, nsz)
